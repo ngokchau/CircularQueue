73ef3a18f5cd4551a76b6782297e91ac4d5a4f84
Chau Ngo <ngokchau@gmail.com>
2015-01-11 14:05:58 -0800

    remove iml

--- Executor.java
+++ Executor.java
@@ -42,15 +42,6 @@ public class Executor {
 				System.out.println((i + 1) + " " + question + ": " + answers[i]);
 			}
 		}
-
-
-		//TODO Assign questions to oracles using Utility.random and print the question paired with the oracle response
-
-		// 1. Initialize one ArrayQueue per oracle (arrays will work best).
-
-		// 2. Put the questions into the queues, assigning each one to the queue of the oracle whose number is returned by the random number generator.
-
-		// 3. Loop through the oracles, having each one remove a question from its queue (if empty do nothing) and answer it with its unique answer (oracle[k] uses answers[k]). Do this repeatedly till all queues become empty.
 	}
 }
 
